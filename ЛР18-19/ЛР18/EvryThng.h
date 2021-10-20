ee1877e2f570efd193d9dbdb79d79a8031e6cdea
Grekov-Igor <igor.grekov03@mail.ru>
2021-10-20 10:08:29 +0300

    ЛР18
    ЛР18

---
+++ ЛР18-19/ЛР18/EvryThng.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+/* EvryThng.h � �������� ��� ����������� � ���������������� */
+
+/* ������������ �����. */
+
+#include "Exclude.h" /* ��������� ��������, ������� �� ��������� ��� �������� ��������.*/ 
+
+#include "envirmnt.h" 
+
+#include <windows.h> 
+
+#include <tchar.h> 
+
+#include <stdio.h> 
+
+#include <io.h> 
+
+#include "support.h" 
+
+#ifdef _MT
+//#define _MT
+
+#include <process.h> 
+
+#if !defined(_Wp64) 
+
+#define DWORD_PTR DWORD 
+
+#define LONG_PTR LONG 
+
+#define INT_PTR INT 
+
+#endif
+#endif
