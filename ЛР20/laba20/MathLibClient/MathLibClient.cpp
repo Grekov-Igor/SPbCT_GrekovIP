7643f3db2c7e77dd3e38530273d4ae8db2dc9381
Grekov-Igor <igor.grekov03@mail.ru>
2021-10-21 20:59:45 +0300

    ЛР20
    ЛР20

---
+++ ЛР20/laba20/MathLibClient/MathLibClient.cpp
@@ -0,0 +1,26 @@
+﻿#include "MathLib.h"
+#include <iostream>
+#include <locale.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    double a, b, c;
+    cout << "Введите сторону а: ";
+    cin >> a;
+    cout << "Введите сторону b: ";
+    cin >> b;
+    cout << "Введите сторону c: ";
+    cin >> c;
+
+    try
+    {
+        cout << "Периметр равен: " << MathFuncs::MyMathFuncs::Per(a, b, c) << endl;
+        cout << "Площадь равна: " << MathFuncs::MyMathFuncs::Square(a, b, c) << endl;
+    }
+    catch (const invalid_argument& e)
+    {
+        cout << "Caught exception: " << e.what() << endl;
+    }
+}
+
