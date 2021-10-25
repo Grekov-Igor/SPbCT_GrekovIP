2739c91e94ced8dde5cc69ebc661eeb1e8ab484b
Grekov-Igor <igor.grekov03@mail.ru>
2021-10-25 10:54:41 +0300

    ЛР21
    ЛР21

---
+++ ЛР21/laba21/WinFix.h
@@ -0,0 +1,34 @@
+#pragma once
+
+
+#include "Exclude.h" 
+
+#include "Environment.h" 
+
+#include <windows.h> 
+
+#include <tchar.h> 
+
+#include <stdio.h> 
+
+#include <io.h> 
+
+#include "Support.h" 
+
+#ifdef _MT
+//#define _MT
+
+#include <process.h> 
+
+
+
+#if !defined(_Wp64) 
+
+#define DWORD_PTR DWORD 
+
+#define LONG_PTR LONG 
+
+#define INT_PTR INT 
+
+#endif
+#endif
